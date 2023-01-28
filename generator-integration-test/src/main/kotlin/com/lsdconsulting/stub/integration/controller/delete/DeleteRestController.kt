336a8dcc9769d6194891c64f3a4962b423cb24b6
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-01-28 11:29:03 +0000

    test: TODOs fixed

--- generator-integration-test/src/main/kotlin/com/lsdconsulting/stub/integration/controller/delete/DeleteRestController.kt
+++ generator-integration-test/src/main/kotlin/com/lsdconsulting/stub/integration/controller/delete/DeleteRestController.kt
@@ -4,10 +4,8 @@ import com.lsdconsulting.stub.integration.model.GreetingRequest
 import com.lsdconsulting.stub.integration.model.GreetingResponse
 import io.lsdconsulting.stub.annotation.GenerateWireMockStub
 import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus.NO_CONTENT
+import org.springframework.web.bind.annotation.*
 
 @GenerateWireMockStub
 @RestController("/deleteController")
@@ -20,6 +18,7 @@ class DeleteRestController {
     fun resourceWithRequestBody(@Suppress("UNUSED_PARAMETER") @RequestBody greetingRequest: GreetingRequest) {
     }
 
+    @ResponseStatus(NO_CONTENT)
     @DeleteMapping("/resourceWithRequestBodyAndPathVariable/{param}")
     fun resourceWithRequestBodyAndPathVariable(
         @Suppress("UNUSED_PARAMETER") @RequestBody greetingRequest: GreetingRequest,
--- generator-integration-test/src/test/kotlin/com/lsdconsulting/stub/integration/delete/DeleteRestControllerIT.kt
+++ generator-integration-test/src/test/kotlin/com/lsdconsulting/stub/integration/delete/DeleteRestControllerIT.kt
@@ -5,10 +5,15 @@ import com.github.tomakehurst.wiremock.client.VerificationException
 import com.lsdconsulting.stub.integration.BaseRestControllerIT
 import com.lsdconsulting.stub.integration.DELETE_CONTROLLER_URL
 import com.lsdconsulting.stub.integration.controller.delete.DeleteRestControllerStub
-import org.junit.jupiter.api.Disabled
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.nullValue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod.DELETE
+import org.springframework.http.HttpStatus.NO_CONTENT
+import org.springframework.http.HttpStatus.OK
 import org.springframework.util.LinkedMultiValueMap
 
 class DeleteRestControllerIT : BaseRestControllerIT() {
@@ -25,24 +30,31 @@ class DeleteRestControllerIT : BaseRestControllerIT() {
     }
 
     @Test
-    @Disabled
     fun `should handle delete mapping with request body`() {
         underTest.verifyDeleteResourceWithRequestBodyNoInteraction(greetingRequest)
         underTest.deleteResourceWithRequestBody()
         val request = HttpEntity(greetingRequest)
-        restTemplate.delete("$DELETE_CONTROLLER_URL/resourceWithRequestBody", request) // TODO Fix delete with body
+        val responseEntity = restTemplate.exchange("$DELETE_CONTROLLER_URL/resourceWithRequestBody", DELETE, request, Unit::class.java)
+        assertThat(responseEntity.body, `is`(nullValue()))
+        assertThat(responseEntity.statusCode, `is`(OK))
         underTest.verifyDeleteResourceWithRequestBody(1, greetingRequest)
         underTest.verifyDeleteResourceWithRequestBody(greetingRequest)
         assertThrows<VerificationException> { underTest.verifyDeleteResourceWithRequestBodyNoInteraction(greetingRequest) }
     }
 
     @Test
-    @Disabled
     fun `should handle delete mapping with request body and path variable`() {
         underTest.verifyDeleteResourceWithRequestBodyAndPathVariableNoInteraction(param, greetingRequest)
         underTest.deleteResourceWithRequestBodyAndPathVariable(param)
         val request = HttpEntity(greetingRequest)
-        restTemplate.delete("$DELETE_CONTROLLER_URL/resourceWithRequestBodyAndPathVariable/$param", request) // TODO Fix delete with body
+        val responseEntity = restTemplate.exchange(
+            "$DELETE_CONTROLLER_URL/resourceWithRequestBodyAndPathVariable/$param",
+            DELETE,
+            request,
+            Unit::class.java
+        )
+        assertThat(responseEntity.body, `is`(nullValue()))
+        assertThat(responseEntity.statusCode, `is`(NO_CONTENT))
         underTest.verifyDeleteResourceWithRequestBodyAndPathVariable(1, param, greetingRequest)
         underTest.verifyDeleteResourceWithRequestBodyAndPathVariable(param, greetingRequest)
         assertThrows<VerificationException> { underTest.verifyDeleteResourceWithRequestBodyAndPathVariableNoInteraction(param, greetingRequest) }
