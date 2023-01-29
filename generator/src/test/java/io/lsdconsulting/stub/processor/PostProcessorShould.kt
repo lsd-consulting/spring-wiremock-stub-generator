6bf68615b234b2407af0307a614079265f647fda
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-01-29 19:56:39 +0000

    test: Unit tests for updating subresource in post-processing

--- generator/src/test/java/io/lsdconsulting/stub/processor/PostProcessorShould.kt
+++ generator/src/test/java/io/lsdconsulting/stub/processor/PostProcessorShould.kt
@@ -1,5 +1,6 @@
 package io.lsdconsulting.stub.processor
 
+import io.lsdconsulting.stub.model.ArgumentModel
 import io.lsdconsulting.stub.model.ControllerModel
 import io.lsdconsulting.stub.model.Model
 import io.lsdconsulting.stub.model.ResourceModel
@@ -27,7 +28,7 @@ internal class PostProcessorShould {
 
         underTest.update(model)
 
-        model.controllers.values.forEach { controller->
+        model.controllers.values.forEach { controller ->
             controller.resources.values.forEach {
                 assertThat(it.responseStatus, `is`(responseStatus))
             }
@@ -78,5 +79,40 @@ internal class PostProcessorShould {
             model.controllers.values.first().resources.values,
             hasItem(hasProperty<ResourceModel>("responseStatus", equalTo(resourceResponseStatus)))
         )
+    }
+
+    @Test
+    fun `update resource URL when URL has path variables`() {
+        val controllerModel = ControllerModel(responseStatus = responseStatus)
+        val resourceModel =
+            ResourceModel(
+                urlHasPathVariable = true,
+                subResource = "/subResource/{param1}/{param2}",
+                pathVariables = mutableMapOf("1" to ArgumentModel("param1"), "2" to ArgumentModel("param2"))
+            )
+        controllerModel.resources["1"] = resourceModel
+        val model = Model()
+        model.controllers["someClass"] = controllerModel
+
+        underTest.update(model)
+
+        assertThat(model.controllers.values.first().resources.values.first().subResource, `is`("/subResource/%s/%s"))
+    }
+
+    @Test
+    fun `keep original resource URL when URL does not have path variables`() {
+        val controllerModel = ControllerModel(responseStatus = responseStatus)
+        val resourceModel =
+            ResourceModel(
+                urlHasPathVariable = false,
+                subResource = "/subResource/{param1}/{param2}"
+            )
+        controllerModel.resources["1"] = resourceModel
+        val model = Model()
+        model.controllers["someClass"] = controllerModel
+
+        underTest.update(model)
+
+        assertThat(model.controllers.values.first().resources.values.first().subResource, `is`("/subResource/{param1}/{param2}"))
     }
 }
