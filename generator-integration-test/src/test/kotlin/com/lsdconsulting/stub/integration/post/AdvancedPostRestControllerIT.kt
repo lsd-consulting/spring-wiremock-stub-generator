0ca5fabe5d891799e514a9cdc642ec2177f630a9
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-02-21 23:57:42 +0000

    fix: Add a new generated method for a POST with a mapping matching the request body

--- generator-integration-test/src/test/kotlin/com/lsdconsulting/stub/integration/post/AdvancedPostRestControllerIT.kt
+++ generator-integration-test/src/test/kotlin/com/lsdconsulting/stub/integration/post/AdvancedPostRestControllerIT.kt
@@ -5,13 +5,17 @@ import com.github.tomakehurst.wiremock.client.VerificationException
 import com.lsdconsulting.stub.integration.BaseRestControllerIT
 import com.lsdconsulting.stub.integration.POST_CONTROLLER_URL
 import com.lsdconsulting.stub.integration.controller.post.AdvancedPostRestControllerStub
+import com.lsdconsulting.stub.integration.model.GreetingRequest
 import com.lsdconsulting.stub.integration.model.GreetingResponse
+import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.notNullValue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.web.client.HttpClientErrorException
 
 class AdvancedPostRestControllerIT : BaseRestControllerIT() {
     private val underTest = AdvancedPostRestControllerStub(ObjectMapper())
@@ -31,6 +35,38 @@ class AdvancedPostRestControllerIT : BaseRestControllerIT() {
         underTest.verifyPostResourceWithBodyAndPathVariableNoInteraction(param, greetingRequest)
         assertThrows<VerificationException> { underTest.verifyPostResourceWithBodyNoInteraction(greetingRequest) }
         assertThrows<VerificationException> { underTest.verifyPostResourceWithBodyNoInteractionWithUrl() }
+    }
+
+    @Test
+    fun `should stub a post method with a mapping matching the request body`() {
+        val additionalGreetingRequest = GreetingRequest(name = randomAlphabetic(10))
+        underTest.verifyPostResourceWithBodyNoInteraction(greetingRequest)
+        underTest.verifyPostResourceWithBodyNoInteraction(additionalGreetingRequest)
+        underTest.verifyPostResourceWithBodyNoInteractionWithUrl()
+        underTest.postResourceWithBody(greetingRequest, greetingResponse)
+
+        val response1 = restTemplate.postForEntity(
+            "$POST_CONTROLLER_URL/resourceWithBody",
+            HttpEntity(greetingRequest),
+            GreetingResponse::class.java
+        )
+        assertThat(response1.body, notNullValue())
+        assertThat(response1.body?.name, `is`(name))
+
+        val exception = assertThrows<HttpClientErrorException> {
+            restTemplate.postForEntity(
+                "$POST_CONTROLLER_URL/resourceWithBody",
+                HttpEntity(additionalGreetingRequest),
+                GreetingResponse::class.java
+            )
+        }
+        assertThat(exception.statusCode, `is`(NOT_FOUND))
+
+
+        underTest.verifyPostResourceWithBody(1, greetingRequest)
+        underTest.verifyPostResourceWithBody(1, additionalGreetingRequest)
+        underTest.verifyPostResourceWithBody(greetingRequest)
+        underTest.verifyPostResourceWithBody(additionalGreetingRequest)
     }
 
     @Test
