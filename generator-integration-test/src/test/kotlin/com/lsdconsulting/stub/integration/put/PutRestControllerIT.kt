009953a6d7b2101eb1d9a56e8d8e026d2bac192c
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-02-22 00:15:44 +0000

    fix: The generated method with a mapping matching the request body should handle PUT

--- generator-integration-test/src/test/kotlin/com/lsdconsulting/stub/integration/put/PutRestControllerIT.kt
+++ generator-integration-test/src/test/kotlin/com/lsdconsulting/stub/integration/put/PutRestControllerIT.kt
@@ -5,6 +5,8 @@ import com.github.tomakehurst.wiremock.client.VerificationException
 import com.lsdconsulting.stub.integration.BaseRestControllerIT
 import com.lsdconsulting.stub.integration.PUT_CONTROLLER_URL
 import com.lsdconsulting.stub.integration.controller.put.PutRestControllerStub
+import com.lsdconsulting.stub.integration.model.GreetingRequest
+import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.nullValue
@@ -12,9 +14,9 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod.PUT
-import org.springframework.http.HttpStatus.ACCEPTED
-import org.springframework.http.HttpStatus.NO_CONTENT
+import org.springframework.http.HttpStatus.*
 import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.HttpClientErrorException
 
 class PutRestControllerIT : BaseRestControllerIT() {
     private val underTest = PutRestControllerStub(ObjectMapper())
@@ -38,6 +40,29 @@ class PutRestControllerIT : BaseRestControllerIT() {
         val responseEntity = restTemplate.exchange("$PUT_CONTROLLER_URL/resourceWithRequestBody", PUT, request, Unit::class.java)
         assertThat(responseEntity.body, `is`(nullValue()))
         assertThat(responseEntity.statusCode, `is`(NO_CONTENT))
+        underTest.verifyPutResourceWithRequestBody(1, greetingRequest)
+        underTest.verifyPutResourceWithRequestBody(greetingRequest)
+        assertThrows<VerificationException> { underTest.verifyPutResourceWithRequestBodyNoInteraction(greetingRequest) }
+    }
+
+    @Test
+    fun `should stub a put method with a mapping matching the request body`() {
+        val additionalGreetingRequest = GreetingRequest(name = randomAlphabetic(10))
+        underTest.verifyPutResourceWithRequestBodyNoInteraction(greetingRequest)
+        underTest.putResourceWithRequestBody(greetingRequest)
+
+        val responseEntity1 = restTemplate.exchange(
+            "$PUT_CONTROLLER_URL/resourceWithRequestBody",
+            PUT,
+            HttpEntity(greetingRequest),
+            Unit::class.java
+        )
+        assertThat(responseEntity1.body, `is`(nullValue()))
+        assertThat(responseEntity1.statusCode, `is`(NO_CONTENT))
+
+        val exception = assertThrows<HttpClientErrorException> { restTemplate.exchange("$PUT_CONTROLLER_URL/resourceWithRequestBody", PUT, HttpEntity(additionalGreetingRequest), Unit::class.java) }
+        assertThat(exception.statusCode, `is`(NOT_FOUND))
+
         underTest.verifyPutResourceWithRequestBody(1, greetingRequest)
         underTest.verifyPutResourceWithRequestBody(greetingRequest)
         assertThrows<VerificationException> { underTest.verifyPutResourceWithRequestBodyNoInteraction(greetingRequest) }
