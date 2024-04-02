6baa0f73d1c1857ad8fef46ac49591a63acbd55e
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2024-04-02 10:09:18 +0100

    build: Upgrade WireMock

--- generator-integration-test/src/main/kotlin/com/lsdconsulting/stub/integration/controller/post/JavaPostRestController.java
+++ generator-integration-test/src/main/kotlin/com/lsdconsulting/stub/integration/controller/post/JavaPostRestController.java
@@ -3,10 +3,13 @@ package com.lsdconsulting.stub.integration.controller.post;
 import com.lsdconsulting.stub.integration.model.GreetingRequest;
 import com.lsdconsulting.stub.integration.model.GreetingResponse;
 import io.lsdconsulting.stub.annotation.GenerateWireMockStub;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Email;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
